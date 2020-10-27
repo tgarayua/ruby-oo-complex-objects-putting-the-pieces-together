abb9c776164e619dbf140afc9143db1df7f63597
tgarayua <tgarayua@gmail.com>
2020-10-27 17:43:47 -0400

    updated

--- lib/book.rb
+++ lib/book.rb
@@ -1,45 +1,38 @@
 class Book
     
+  attr_accessor :title
 
-    def initialize(title)
-        @title = title
+  def initialize(title)
+      @title = title
+  end
+
+  def author=(author)
+      @author = author
     end
 
-    def title
-        @title
+    def author
+      @author
     end
 
-    def author=(author)
-        @author = author
-      end
+    def page_count=(num)
+      @page_count = num
+    end
+  
+    def page_count
+      @page_count
+    end
 
-      def author
-        @author
-      end
+    def genre=(genre)
+      @genre = genre
+    end
+  
+    def genre
+      @genre
+    end
 
-      def page_count=(num)
-        @page_count = num
-      end
-    
-      def page_count
-        @page_count
-      end
-
-      def genre=(genre)
-        @genre = genre
-      end
-    
-      def genre
-        @genre
-      end
-
-      def turn_page
-        puts "Flipping the page...wow, you read fast!"
-      end
-    
-      book = Book.new("Some Title")
-book.turn_page
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
 end
-
-
-new_book = Book.new("And Then There Were None")
+book = Book.new("Some Title")
+book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,48 +1,45 @@
-
 class Shoe
+    
+    attr_accessor :brand
+  
     def initialize(brand)
         @brand = brand
     end
-
-    def brand
-        @brand
-    end
-
+  
     def color=(color)
         @color = color
-    end
-
-    def color
+      end
+  
+      def color
         @color
-    end
-
-    def size=(size)
-        @size = size
-    end
-
-    def size
+      end
+  
+      def size=(num)
+        @size = num
+      end
+    
+      def size
         @size
-    end
-
-    def material=(material)
+      end
+  
+      def material=(material)
         @material = material
-    end
-
-    def material
+      end
+    
+      def material
         @material
-    end
+      end
 
-    def condition=(condition)
+      def condition=(condition)
         @condition = condition
-    end
-
-    def condition
+      end
+    
+      def condition
         @condition
-    end
-
-    def cobble
+      end
+  
+      def cobble
         @condition = "new"
         puts "Your shoe is as good as new!"
-    end
-
-end
+      end
+  end
